54ab719ef1a5498bb8c26c047ed83df8e3b285d9
Deepu Mohan Puthrote <git@deepumohan.com>
2023-11-04 13:16:25 +0000

    Initial commit

---
+++ test/warfox/clojure_template_test.clj
@@ -0,0 +1,7 @@
+(ns warfox.clojure-template-test
+  (:require [clojure.test :refer :all]
+            [warfox.clojure-template :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
